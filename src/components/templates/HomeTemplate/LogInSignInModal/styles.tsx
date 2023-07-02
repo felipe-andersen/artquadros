db04bee19ba5f5fa35baf51e78f67a3756dc4791
felipe <felipe.coutinho.psl@gmail.com>
2023-07-02 20:43:53 -0300

    [feat] add login signin modal

---
+++ src/components/templates/HomeTemplate/LogInSignInModal/styles.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import styled from "styled-components";
+
+const Div = styled.div`
+box-sizing: border-box;
+position: absolute;
+height: 100vh;
+width: 100vw;
+background-color: rgba(0,0,0,0.3);
+dispĺay: flex;
+flex-direction: column;
+align-items: center;
+
+section {
+    dispĺay: flex;
+    flex-direction: column;
+align-items: center;
+background-color: rgba(255,255,255,1);
+}
+
+
+
+
+
+`;
+
+
+export default Div;
