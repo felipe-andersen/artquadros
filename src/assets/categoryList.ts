98e18fd5a11631c104ecbe4709b49bdb766c3fda
felipe <felipe.coutinho.psl@gmail.com>
2023-08-01 19:54:58 -0300

    create assets folder

---
+++ src/assets/categoryList.ts
@@ -0,0 +1,11 @@
+
+const data = [ 
+    {name: "lettering", pic: "", alt: "", lang:"pt-BR"},
+    {name: "folhas e cacto", alt: "", pic: "", lang:"pt-BR"},
+    {name: "black & white", alt: "", pic: "", lang:"pt-BR"},
+    {name: "dinossauros", alt: "", pic: "", lang:"pt-BR"},
+    {name: "dourados", alt: "", pic: "", lang:"pt-BR"},
+    {name: "dinossauros", alt: "", pic: "", lang:"pt-BR"},
+    {name: "dourados", alt: "", pic: "", lang:"pt-BR"},
+];
+
