ac4df893ef594e9fa8bca6408d41e784401c1daf
felipe <felipe.coutinho.psl@gmail.com>
2023-08-03 22:52:10 -0300

    :truck :chore Rename hostingHTTP > baseURL

---
+++ src/utils/baseURL/index.tsx
@@ -0,0 +1,14 @@
+const hostingHTTP = `http://localhost:3000`
+
+export default hostingHTTP
+
+/*
+    /t./g  t e qualquer outro caractere
+    /()/g  array
+
+    /(i|I)/g  ou
+    /(^et)/g menos ou negar
+
+
+
+*/
